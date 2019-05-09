255f02dfb6a011de11ec8ccdb6017ad27cb7fee6
scottminter <sminter@gmail.com>
2019-05-09 16:12:18 -0400

    added variables and scope

---
+++ scope.js
@@ -0,0 +1,65 @@
+/**
+ * Scopes
+ */
+
+/* Local */
+let func = function () {
+  let loc = 'i am local';
+  console.log(loc);
+}
+
+func();
+console.log(loc);
+
+
+/* Global */
+// let bar = 4;
+// let func2 = function () {
+//   console.log(bar);
+//   bar = 50;
+// }
+
+// console.log(bar);
+// func2();
+// console.log(bar);
+
+
+
+/**
+ * Parent vs Child Scope
+ * 
+ * Child can access the parent scope.
+ * Parent cannot access the child scope.
+ */
+// let g = 'global';
+
+// function blender (fruit) {
+//   let b = fruit,
+//     y = 'yogurt';
+
+//     function blendSmoothie () {
+//       let size = 'lg';
+//       console.log(b + ' and ' + y + ' makes ' + size + ' ' + b + ' swirl');
+//     }
+//     // console.log(size);
+//     blendSmoothie();
+// }
+
+// blender('blueberry');
+// // console.log(size);
+
+
+/**
+ * Precedence
+ */
+
+// var g = 'global';
+
+// function go () {
+//   let g = 'local';
+
+//   console.log(g + ' inside go');
+// }
+
+// go();
+// console.log(g + ' outside go');
--- variables.js
+++ variables.js
@@ -1,2 +1,46 @@
-// CONST vs VAR vs LET
+/**
+ * Const vs Let vs Var
+ * Var is function scoped
+ * Let is block scoped
+ */
+// calling x after definition 
+var x = 5;
+console.log(x);
+
+// calling y after definition  
+let y = 10;
+console.log(y);
+
+// calling var z before definition will return undefined 
+console.log(z);
+var z = 2;
+
+// calling let a before definition will give error 
+console.log(a);
+let a = 3;
+
+// using let is best practice for Node version 6^
+// this is also a good reason to declare all variables at the top of a function
+function foobar () {
+  let a = 1,
+    b = 2,
+    c = 3;
+
+    return a + b + c;
+}
+
+console.log(foobar());
+ 
+
+
+
+/**
+ * Const
+ * It can't be reassigned
+ */
+const myConst = 'some value';
+console.log(myConst);
+
+myConst = 'a new value';
+console.log(myConst);
 
